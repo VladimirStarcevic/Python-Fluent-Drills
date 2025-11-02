5b6d20e07e041cf97e4f43f0994e8bc4f1303900
VladimirStarcevic <vladimirstarcevic33@gmail.com>
2025-11-02 22:49:23 +0100

    feat: Completed drills 1-15 in review. Demonstrated full fluency in LC, DC, Lambda, enumerate, and zip.

---
+++ practice_files/practice_25_1.py
@@ -0,0 +1,241 @@
+# Zadatak 1: Kvadriranje parnih brojeva
+# Napiši funkciju square_even_numbers koja prihvata listu brojeva.
+# Koristeći list comprehension, funkcija treba da isfiltrira samo parne brojeve iz ulazne liste i vrati novu listu koja sadrži njihove kvadrate.
+#
+# Primer ulaza: [1, 2, 3, 4, 5, 6, 7]
+# Očekivani izlaz: [4, 16, 36]
+
+def square_even_numbers(list_in: list[int]) -> list[int]:
+
+    return [num**2 for num in list_in if num % 2 == 0]
+
+
+squared_nums = square_even_numbers([1, 2, 3, 4, 5, 6, 7])
+print(squared_nums)
+
+# Zadatak 2: Sortiranje jedinstvenih reči po dužini
+# Napiši funkciju sort_unique_by_length koja prihvata listu reči.
+# Funkcija treba prvo da ukloni sve duplikate, a zatim da sortira jedinstvene reči po njihovoj dužini, od najkraće do najduže.
+#
+# Primer ulaza: ['apple', 'banana', 'cucumber', 'apple', 'pear']
+# Očekivani izlaz: ['pear', 'apple', 'banana', 'cucumber']
+
+def sort_unique_by_length(words: list[str]) -> list[str]:
+
+    unique_ls = set(words)
+
+    return sorted(unique_ls, key=lambda word: len(word))
+
+sorted_words = sort_unique_by_length(['apple', 'banana', 'cucumber', 'apple', 'pear'])
+print(sorted_words)
+# Zadatak 3: Inverzija brojeva u stringu
+# Napiši funkciju invert_and_keep_string koja prihvata listu stringova,
+# gde svaki string predstavlja ceo broj. Koristeći list comprehension,
+#funkcija treba da promeni znak svakom broju (pozitivan postaje negativan i obrnuto) i vrati novu listu sa promenjenim vrednostima, koje takođe moraju biti tipa string.
+#
+# Primer ulaza: ["10", "-5", "2", "-1"]
+# Očekivani izlaz: ['-10', '5', '-2', '1']
+
+def invert_and_keep_string(string_ls: list[str]) -> list[str]:
+
+    return [str(-int(digit_str)) for digit_str in string_ls]
+
+inverted_list = invert_and_keep_string(["10", "-5", "2", "-1"])
+print(inverted_list)
+
+# Zadatak 4: Pronalaženje najjeftinijeg proizvoda
+# Napiši funkciju find_cheapest_product koja prihvata listu torki (tuples). Svaka torka se sastoji od naziva proizvoda (string) i njegove cene (broj). Koristeći min() funkciju i lambda izraz, pronađi i vrati kompletnu torku koja predstavlja najjeftiniji proizvod.
+#
+# Primer ulaza: [('Laptop', 1200), ('Monitor', 300), ('Mouse', 15), ('Keyboard', 50)]
+# Očekivani izlaz: ('Mouse', 15)
+
+def find_cheapest_product(ls_tuples: list[tuple[str, int]]) -> tuple[str, int]:
+
+    return min(ls_tuples, key=lambda item: item[1])
+
+low_price_product = find_cheapest_product([('Laptop', 1200), ('Monitor', 300), ('Mouse', 15), ('Keyboard', 50)])
+print(low_price_product)
+# Zadatak 5: Mapiranje liste u rečnik sa indeksima
+# Napiši funkciju index_to_dictionary koja prihvata listu stavki. Koristeći petlju i enumerate, kreiraj i vrati rečnik (dictionary) gde su ključevi indeksi elemenata iz ulazne liste, a vrednosti su sami elementi.
+#
+# Primer ulaza: ['milk', 'bread', 'eggs']
+# Očekivani izlaz: {1: 'milk', 2: 'bread', 3: 'eggs'}
+
+def index_to_dictionary(product_ls: list[str]) -> dict[int, str]:
+
+    return {
+        idx + 1: product for idx,product in enumerate(product_ls)
+    }
+
+dc_out = index_to_dictionary(['milk', 'bread', 'eggs'])
+print(dc_out)
+# Zadatak 6: Filtriranje ocena
+# Napiši funkciju filter_high_grades koja prihvata dve liste iste dužine: listu sa imenima učenika i listu sa njihovim ocenama. Koristeći zip funkciju i list comprehension,
+# spoji svakog učenika sa njegovom ocenom i vrati novu listu formatiranih stringova, ali samo za one učenike čija je ocena veća od 80.
+#
+# Primer ulaza: names=['Pera', 'Mika', 'Laza', 'Ana'], grades=[95, 78, 88, 65]
+# Očekivani izlaz: ['Pera has 95', 'Laza has 88']
+
+def filter_high_grades(names_ls: list[str], grades_ls: list[int]) -> list[str]:
+
+    return [f"{name} has {grade}" for name, grade in zip(names_ls, grades_ls) if grade > 80]
+
+filtered_list = filter_high_grades( ['Pera', 'Mika', 'Laza', 'Ana'],[95, 78, 88, 65])
+
+print(filtered_list)
+
+# Zadatak 7: Uslovna zamena vrednosti ("Fizz" zadatak)
+# Napiši funkciju fizz_swap koja prihvata listu brojeva. Koristeći list comprehension i ternarni operator,
+# funkcija treba da vrati novu listu u kojoj je svaki broj deljiv sa 3 zamenjen stringom "Fizz". Ostali brojevi treba da ostanu nepromenjeni.
+#
+# Primer ulaza: [1, 3, 5, 6, 8, 9, 10]
+# Očekivani izlaz: [1, 'Fizz', 5, 'Fizz', 8, 'Fizz', 10]
+
+def fizz_swap(numbers):
+
+    return [
+        "Fizz" if num % 3 == 0 else num for num in numbers
+    ]
+
+fizzy_list = fizz_swap([1, 3, 5, 6, 8, 9, 10])
+print(fizzy_list)
+
+# Zadatak 8: Povećanje cena za skuplje proizvode
+# Napiši funkciju price_markup koja prihvata listu cena.
+# Koristeći list comprehension,
+# funkcija treba da filtrira samo one cene koje su veće od 100 i da na njih primeni povećanje od 10%.
+# Funkcija treba da vrati novu listu koja sadrži samo preračunate, uvećane cene.
+#
+# Primer ulaza: [50, 120, 99, 200, 10]
+# Očekivani izlaz: [132.0, 220.0]
+
+def price_markup(numeric: list[int]) -> list[float]:
+
+    return [
+        round(num + num*0.1, 2) for num in numeric if num > 100
+    ]
+
+numeric_ls = price_markup([50, 120, 99, 200, 10])
+print(numeric_ls)
+
+# Zadatak 9: Uslovno spajanje rečnika
+# Napiši funkciju conditional_merge koja prihvata dva rečnika, d1 i d2. Funkcija treba da kreira novi rečnik na sledeći način:
+#
+# Prvo, iz rečnika d1 filtriraj i zadrži samo one parove ključ-vrednost gde je vrednost veća od 50.
+# Zatim, spoji taj filtrirani rečnik sa celokupnim rečnikom d2.
+# Ako neki ključ postoji u oba rečnika (u filtriranom d1 i u d2), vrednost iz d2 ima prednost.
+#
+# Ulaz 1: d1 = {'A': 90, 'B': 40, 'C': 70}
+# Ulaz 2: d2 = {'B': 5, 'D': 100, 'A': 10}
+# Očekivani izlaz: {'C': 70, 'B': 5, 'D': 100, 'A': 10}
+
+def conditional_merge(d1: dict[str, int], d2: dict[str, int]) -> dict[str, int]:
+
+    filtered_dict = {k: v for k,v in d1.items() if v > 50}
+    return {**d2,**filtered_dict}
+
+result = conditional_merge({'A': 90, 'B': 40, 'C': 70}, {'B': 5, 'D': 100, 'A': 10})
+print(result)
+
+# Zadatak 10: Agregacija (sabiranje) poena
+# Napiši funkciju aggregate_points koja prihvata listu torki.
+# Svaka torka sadrži ime studenta i broj poena koje je osvojio.
+# Funkcija treba da prođe kroz listu i sabere sve poene za svakog studenta, vraćajući rečnik gde su ključevi imena studenata, a vrednosti ukupan zbir njihovih poena.
+#
+# Primer ulaza: [('Pera', 10), ('Mika', 5), ('Pera', 20), ('Laza', 15)]
+# Očekivani izlaz: {'Pera': 30, 'Mika': 5, 'Laza': 15}
+
+def aggregate_points(students_list: list[tuple[str, int]]) -> dict[str, int]:
+
+    sum_of_grades = {}
+    for name, grade in students_list:
+
+        sum_of_grades[name] = sum_of_grades.get(name, 0) + grade
+    return sum_of_grades
+
+total_grade = aggregate_points([('Pera', 10), ('Mika', 5), ('Pera', 20), ('Laza', 15)])
+print(total_grade)
+
+# Zadatak 11: Kvadriranje vrednosti na parnim indeksima
+# Napiši funkciju square_at_even_indices koja prihvata listu brojeva.
+# Koristeći enumerate i list comprehension,
+# funkcija treba da vrati novu listu koja sadrži kvadrate samo onih brojeva koji se u originalnoj listi nalaze na parnim indeksima (0, 2, 4, itd.).
+#
+# Primer ulaza: [1, 2, 3, 4, 5, 6]
+# Očekivani izlaz: [1, 9, 25]
+
+def square_at_even_indices(numbs: list[int]) -> list[int]:
+
+    return [
+        num**2 for idx, num in enumerate(numbs) if idx % 2 == 0
+    ]
+
+squared_ls = square_at_even_indices([1, 2, 3, 4, 5, 6])
+print(squared_ls)
+
+# Zadatak 12: Sortiranje po ugnežđenim vrednostima
+# Napiši funkciju sort_by_nested_tuple koja prima listu torki.
+# Svaka torka sadrži ime osobe i drugu (ugnežđenu) torku sa godinama i visinom te osobe ('Ime', (godine, visina)).
+# Sortiraj listu po sledećim kriterijumima:
+#
+# Primarno po godinama, rastuće.
+# Sekundarno po visini, opadajuće (ako su godine iste).
+# Primer ulaza: [('Ana', (25, 170)), ('Pera', (30, 180)), ('Mika', (25, 175))]
+# Očekivani izlaz: [('Mika', (25, 175)), ('Ana', (25, 170)), ('Pera', (30, 180))]
+
+def sort_by_nested_tuple(data):
+
+    return sorted(data, key=lambda item: (item[1][0], -item[1][1]))
+
+sorted_data = sort_by_nested_tuple([('Ana', (25, 170)), ('Pera', (30, 180)), ('Mika', (25, 175))])
+print(sorted_data)
+
+# Zadatak 13: Zamena malih decimalnih brojeva
+# Napiši funkciju filter_small_floats koja prihvata listu decimalnih brojeva.
+# Koristeći list comprehension i ternarni operator,
+# vrati novu listu gde je svaki broj veći od 10.0 zadržan, dok je svaki broj manji ili jednak 10.0 zamenjen sa 0.0.
+#
+# Primer ulaza: [15.5, 9.2, 11.0, 5.0, 10.0]
+# Očekivani izlaz: [15.5, 0.0, 11.0, 0.0, 0.0]
+
+def filter_small_floats(numeric_lst: list[float]) -> list[float]:
+
+    return [
+        0.0 if num <= 10.0 else num for num in numeric_lst
+    ]
+
+small_ls = filter_small_floats([15.5, 9.2, 11.0, 5.0, 10.0])
+print(small_ls)
+
+# Zadatak 14: Filtriranje reči po dužini i početnom slovu
+# Napiši funkciju filter_by_length_and_start koja prima listu reči.
+# Koristeći list comprehension, vrati novu listu koja sadrži samo one reči koje istovremeno ispunjavaju oba uslova: duže su od 5 slova I počinju velikim slovom 'C'.
+#
+# Primer ulaza: ['apple', 'Cucumber', 'cat', 'Computer', 'caravan']
+# Očekivani izlaz: ['Cucumber', 'Computer', 'caravan']
+
+def filter_by_length_and_start(words_list: list[str]) -> list[str]:
+
+    return [
+        word for word in words_list if len(word) > 5 and word.startswith('C')
+    ]
+
+cond_ls_words = filter_by_length_and_start(['apple', 'Cucumber', 'cat', 'Computer', 'caravan'])
+print(cond_ls_words)
+
+# Zadatak 15: Inverzija vrednosti u rečniku
+# Napiši funkciju invert_dictionary_values koja prihvata rečnik sa numeričkim vrednostima.
+# Koristeći dictionary comprehension, kreiraj i vrati novi rečnik gde su ključevi isti kao u ulaznom rečniku,
+# ali su sve vrednosti pomnožene sa -1.
+#
+# Primer ulaza: {'prihod': 1000, 'trosak': 500, 'dobit': 500}
+# Očekivani izlaz: {'prihod': -1000, 'trosak': -500, 'dobit': -500}
+
+def invert_dictionary_values(data_map: dict[str, int]) -> dict[str, int]:
+
+    return {
+        key: -1*value for key, value in data_map.items()
+    }
+
+res = invert_dictionary_values({'prihod': 1000, 'trosak': 500, 'dobit': 500})
+print(res)
